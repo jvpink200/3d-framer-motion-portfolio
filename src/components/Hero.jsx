8b4379f12fb1f97a39619529a0fb28f7d3b18b9e
Janet Villarreal <jvillarr20@gmail.com>
2023-09-16 20:23:31 -0700

    updated Hero and navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
@@ -17,13 +19,31 @@ const Hero = () => {
             Hi, I am <span className="text-[#915EFF]">Janet Villarreal</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces <br className="sm:block hidden" />
-            and web-based software applications.
+            I develop 3D visuals, user <br className="sm:block hidden" />
+            interfaces and web applications
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
+
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };

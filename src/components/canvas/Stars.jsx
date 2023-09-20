b658555a0eecef0bb445e609dab797dd3cb4229e
Janet Villarreal <jvillarr20@gmail.com>
2023-09-20 13:38:41 -0700

    fixed stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,9 +5,9 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
-  useFrame((delta) => {
+  useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });

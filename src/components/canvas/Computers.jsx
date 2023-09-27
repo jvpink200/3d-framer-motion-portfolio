91552aa7358513323a615603ea0a8414e00b1d88
Janet Villarreal <jvillarr20@gmail.com>
2023-09-26 22:10:17 -0700

    improved pc position

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,7 +23,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.4 : 0.75}
-        position={isMobile ? [0, -2, -1.0] : [0, -3.25, -1.5]}
+        position={isMobile ? [0, -2.75, -1.0] : [0, -3.25, -1.5]}
         rotation={[-0.01, 0.3, 0.1]}
       />
     </mesh>

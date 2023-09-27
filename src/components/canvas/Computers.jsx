587ed5434e6f6b7704f4e48766a47ac8e5a02cb2
Janet Villarreal <jvillarr20@gmail.com>
2023-09-27 14:00:59 -0700

    updated computers

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -37,18 +37,15 @@ const ComputersCanvas = () => {
     // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
-    // callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
-    // Remove the listener when the component is unmounted
+    //clean up function
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };

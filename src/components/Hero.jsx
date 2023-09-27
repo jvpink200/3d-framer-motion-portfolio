1e2e4c9266dc5bd8364725f8cd535caed3be8af7
Janet Villarreal <jvillarr20@gmail.com>
2023-09-27 14:19:16 -0700

    improved hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 sm:top-[50px] xs:top-[100px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 xl:top-[120px] top-[55px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
@@ -14,7 +14,7 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col justify-center items-center">
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`${styles.heroHeadText} text-white-100`}>
             Hi, I&rsquo;m
             <span className="text-[#915EFF]"> Janet Villarreal</span>
           </h1>
